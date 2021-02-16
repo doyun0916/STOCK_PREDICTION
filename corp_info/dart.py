9de2b8f45fab876c80846f82449d903850ab65e4
doyun0916 <52279390+doyun0916@users.noreply.github.com>
2021-02-16 18:25:33 +0900

    corps_Data with amount 200

--- corp_info/dart.py
+++ corp_info/dart.py
@@ -1,4 +1,6 @@
 import dart_fss as dart
+import pandas as pd
+from datetime import datetime, timedelta
 
 # Open DART API KEY 설정
 api_key='8a21428e7aff607d8bfb0963894c7a030650cf4e'
@@ -7,19 +9,26 @@ dart.set_api_key(api_key=api_key)
 # DART 에 공시된 회사 리스트 불러오기
 corp_list = dart.get_corp_list()
 
-# 삼성전자 검색
-corp_name = corp_list.find_by_corp_name('삼성전자', exactly=True)[0]
-corp_nums = None
+# 검색
+name = input("회사이름: ")
+corp_name = corp_list.find_by_corp_name(name, exactly=True)[0]
 corp_name = str(corp_name)
+corp_nums = None                           # 회사고유코드 담을 변수
 for i in range(1, len(corp_name)):
     if corp_name[i] == "]":
         corp_nums = corp_name[1:i]
         break
-print(corp_nums)
-result = dart.filings.search(corp_code=corp_nums, bgn_de='20200101', last_reprt_at='N', pblntf_ty=None, pblntf_detail_ty=None, sort='date', sort_mth='desc', page_no=3, page_count=50)
-print(result)
-# 2012년부터 연간 연결재무제표 불러오기
-#fs = samsung.extract_fs(bgn_de='20120101')
+start = str(datetime.now() + timedelta(days=-365))[:10]          # (-365, 즉 지금으로부터 1년전 공시자료부터 최신까지 검색을 위한 기간 설정)
+date_start = str(start).replace('-', '').replace(' ', '')
+titles = []
+for i in range(1, 3):
+    result = dart.filings.search(corp_code=corp_nums, bgn_de=str(date_start), last_reprt_at='N',
+                                 pblntf_ty=None, pblntf_detail_ty=None,
+                                 sort='date', sort_mth='desc',
+                                 page_no=i, page_count=100)                            # page_no, page_count로 양 조절 (몇번째 페이지에서 몇개 가져올지)
 
-# 재무제표 검색 결과를 엑셀파일로 저장 ( 기본저장위치: 실행폴더/fsdata )
-#fs.save()
+    for i in range(len(result)):                                                    # 검색 결과를 제목만 뽑아내기
+        titles.append(result.report_list[i].report_nm)
+
+titles = pd.DataFrame(titles)
+titles.to_csv('./gongsi_titles.csv', sep=',', na_rep='NaN', index=False)
